ba12756e87d92641c869535dbef21c7d87390cdb
Aazrahell <scorpix@onet.pl>
2023-10-28 15:12:02 +0200

    sort and print files by lastModified descending

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.io.File;
-import java.util.Arrays;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,11 +8,35 @@ public class Main {
 
         File[] files = dir.listFiles();
 
-        if(dir.isDirectory()){
-            System.out.println("Directory '"+path+"' exist" );
+        if(!dir.isDirectory()){
+            System.out.println("Directory '"+path+"' doesn't exist." );
+            System.exit(0);
         }
         else{
-            System.out.println("Directory '"+path+"' doesn't exist");
+            System.out.println("Directory '"+path+"' exist.");
+
+
+            if (files.length == 0) {
+                System.out.println("Directory is empty.");
+            }
+            else{
+                //sort files
+                for(int i=0; i<files.length-1; i++){
+                    for(int j=0; j<files.length-1; j++){
+                        if(files[i].lastModified() < files[i+1].lastModified()){
+                            File temp = files[i];
+                            files[i] = files[i+1];
+                            files[i+1] = temp;
+                        }
+                    }
+                }
+
+                //print files
+                for (File file : files) {
+                    Date date = new Date(file.lastModified());
+                    System.out.println(file+"  :  "+date);
+                }
+            }
         }
     }
 }
